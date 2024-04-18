b980cfa0950c68cab8ed643ebb70b0cdb76a5b15
Eva Schiller <eva_schiller@brown.edu>
2024-04-18 19:59:42 -0400

    betterdata

--- getlyrics.py
+++ getlyrics.py
@@ -8,7 +8,15 @@ genius = lyricsgenius.Genius(client_id, client_secret)
 
 df = pd.read_csv('songs.csv')
 for i, row in df.iterrows():
-    if row['genre'] == 'Country':
+    if row['genre'] == 'Rock':
+        # filter out songs in this list: popular, fly girl, white crocs, feelings, waiting for you, think fast,
+        # 2 sugar, i remember everything, fantasy, we go down together, what was i made for, dance the night
+
+        remove_list = ["popular", "fly girl", "white crocs", "feelings", "waiting for you", "think fast", "2 sugar",
+                        "i remember everything", "fantasy", "we go down together", "what was i made for", "dance the night"]
+        if any(word in row['track'].lower() for word in remove_list):
+            df = df.drop(i)
+            continue
         song = genius.search_song(row['track'], row['artist'])
         if song:
             df.loc[i, 'lyrics'] = song.lyrics
